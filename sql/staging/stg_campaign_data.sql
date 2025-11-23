e15915c676d524af25d4683e08ab8355a66d9ab8
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-24 03:11:54 +0800

    Update stg_campaign_data.sql

--- sql/staging/stg_campaign_data.sql
+++ sql/staging/stg_campaign_data.sql
@@ -5,3 +5,5 @@ SELECT DISTINCT
     regexp_replace(campaign_description, '^.*-\s*', '') AS campaign_description_person,
     (regexp_replace(discount, '[^0-9\.]', '', 'g')::FLOAT / 100) AS campaign_discount
 FROM {{ ref('clean_campaign_data') }}
+WHERE campaign_id IS NOT NULL
+    campaign_discount IS NOT NULL
