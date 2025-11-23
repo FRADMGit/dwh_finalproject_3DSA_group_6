af0c565af894efea4651668f93b557d89eab55d3
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-24 03:06:25 +0800

    Create stg_order_data.sql

---
+++ sql/staging/stg_order_data.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT
+    order_id,
+    user_id,
+    REGEXP_REPLACE("estimated arrival", '[^0-9\.]', '', 'g') AS order_arrival_days,
+    transaction_date AS order_transaction_date
+FROM
+    {{ ref('clean_order_data') }}
