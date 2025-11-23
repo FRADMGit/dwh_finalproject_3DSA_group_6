8317dc81b0a9b3d50554085841af80a779e62298
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-24 03:28:55 +0800

    Update stg_transactional_campaign_data.sql

--- sql/staging/stg_transactional_campaign_data.sql
+++ sql/staging/stg_transactional_campaign_data.sql
@@ -1,8 +1,6 @@
 SELECT DISTINCT
     campaign_id,
     order_id,
-    transaction_date AS order_transaction_date,
-    REGEXP_REPLACE("estimated arrival", '[^0-9\.]', '', 'g') AS order_arrival_days,
     availed AS campaign_availed
 FROM
     {{ ref('clean_transactional_campaign_data') }}
