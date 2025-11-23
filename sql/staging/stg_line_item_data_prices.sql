25d68c45f362fe31de227686f9dbdace420f8a87
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-24 03:14:23 +0800

    Update stg_line_item_data_prices.sql

--- sql/staging/stg_line_item_data_prices.sql
+++ sql/staging/stg_line_item_data_prices.sql
@@ -5,5 +5,7 @@ SELECT DISTINCT
     REGEXP_REPLACE(quantity, '[^0-9\.]', '', 'g') AS line_quantity
 FROM
     {{ ref('clean_line_item_data_prices') }}
+WHERE "Unnamed: 0" IS NOT NULL
+    AND order_id IS NOT NULL
 ORDER BY
     "Unnamed: 0" ASC
